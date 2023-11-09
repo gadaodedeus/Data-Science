06a33dd863b7a13615a64464cdcbb54b4b2db1f7
Trizio <gabriel-benfatti.campos@unesp.br>
2023-11-09 01:02:10 -0300

    main test

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import pandas as pd
-from init import init_df
 import quality as q 
 import noiser as n
 
-df=pd.read_excel('base.xlsx')
+# leitura do arquivo (csv)
+########################################################
+df=pd.read_csv('disney_movies_1pct.csv')
 
-n.typo(df, 0.2, ['Segmento '], 1)
+# remoção de registros com dados faltando
+########################################################
+# print(len(df))
+# df=df.dropna(axis='rows')
+# print(len(df))
+
+# adição de ruido
+########################################################
+# frac=0.05
+# q.dtype_columns(df)
+
+# df=n.insert_nan(df, frac, df.columns)
+# df=n.typo(df, frac, ['movie_title', 'genre', 'mpaa_rating'])
+# df=n.to_str(df, frac ,['total_gross'])
+# df=n.duplicate_rows(df, frac)
+
+# verificação da qualidade dos dados
+########################################################
+q.dtype_columns(df)
+q.duplicated(df)
+q.missing(df,df.columns)
+
+# salvar no arquivo (csv)
+########################################################
+#df.to_csv('disney_movies_1pct.csv')
--- quality.py
+++ quality.py
@@ -2,19 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
-def pie_plot(labels, percent, title):
-    plot=plt.pie([percent,1-percent],labels=labels,autopct='%1.1f%%')
-    plt.title(title)
+def pie_plot(labels, dict_col, title):
+    l=0
+    fig, axs = plt.subplots(len(dict_col.keys()))
+    fig.suptitle(title)
+
+    if(len(dict_col.keys()) > 1):   # more than one item on dict
+        for i in dict_col.keys():
+            axs[l].pie([dict_col[i], 1-dict_col[i]], labels=labels, autopct='%1.1f%%')
+            axs[l].set_title(i)
+            l+=1
+
+    else:   # one item on dict
+        axs.pie([dict_col['0'], 1-dict_col['0']], labels=labels, autopct='%1.1f%%')
+
     plt.show()
 
-def duplicated(df):
+def duplicated(df): # rows duplicated
     percent_dupli = df.duplicated().sum()/len(df)
     labels=['Duplicados','Únicos']
-    print(df.duplicated().sum()/len(df))
-    pie_plot(labels, percent_dupli, 'Registros Duplicados')
+    pie_plot(labels, {'0': percent_dupli}, 'Registros Duplicados')
 
-def missing(df): #quantidade de registros nao vazios
-    percent_mis = df.isna().sum()/len(df)
+def missing(df, col): # missing values on columns
+    dict_col={}
+    for i in col:
+        percent_mis = df[i].isna().sum()/len(df)
+        dict_col[i]=percent_mis
     labels = ['Faltando', 'Completo']
-    title='Dados faltantes em '+str(df.name)
-    pie_plot(labels, percent_mis, title)
+    pie_plot(labels, dict_col, 'Registros Faltantes')
+
+def dtype_columns(df):  # data type on each row
+    print(df.dtypes)
